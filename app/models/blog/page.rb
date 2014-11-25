0bf88cf6007192969101ad80e7a1a2157cd9ac59
itowtips <ito@web-tips.co.jp>
2014-11-25 14:19:52 +0900

    add sample blog page

---
+++ app/models/blog/page.rb
@@ -0,0 +1,24 @@
+class Blog::Page
+  include Cms::Model::Page
+  include Cms::Addon::Meta
+  include Cms::Addon::Body
+  include Cms::Addon::File
+  include Cms::Addon::Release
+  include Cms::Addon::ReleasePlan
+  include Cms::Addon::RelatedPage
+  include Category::Addon::Category
+  include Workflow::Addon::Approver
+
+  before_save :seq_filename, if: ->{ basename.blank? }
+
+  default_scope ->{ where(route: "blog/page") }
+
+  private
+    def validate_filename
+      (@basename && @basename.blank?) ? nil : super
+    end
+
+    def seq_filename
+      self.filename = dirname ? "#{dirname}#{id}.html" : "#{id}.html"
+    end
+end
