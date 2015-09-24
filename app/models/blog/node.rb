itowtips/shirasagi

add sample blog node

app/models/blog/node.rb
@@ -1,0 +1,14 @@
+module Blog::Node
+  class Base
+    include Cms::Node::Model
+
+    default_scope ->{ where(route: /^blog\//) }
+  end
+
+  class Page
+    include Cms::Node::Model
+    include Cms::Addon::PageList
+
+    default_scope ->{ where(route: "blog/page") }
+  end
+end

saitoytk/ss-handson

app/models/blog/node.rb
@@ -1,0 +1,14 @@
+module Blog::Node
+  class Base
+    include Cms::Model::Node
+
+    default_scope ->{ where(route: /^blog\//) }
+  end
+
+  class Page
+    include Cms::Model::Node
+    include Cms::Addon::PageList
+
+    default_scope ->{ where(route: "blog/page") }
+  end
+end